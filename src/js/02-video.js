cb33670afe0a9fc0aeb14064dcb79811a9618a27
Katharinaad <vermilion.adams@gmail.com>
2023-07-30 22:44:22 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let time = localStorage.getItem('videoplayer-current-time');
+if (time != null) {
+  player.setCurrentTime(time);
+}
+
+player.on(
+  'pause',
+  throttle(function (data) {
+    player.getCurrentTime().then(function (seconds) {
+      localStorage.setItem(
+        'videoplayer-current-time',
+        JSON.stringify(data.seconds)
+      );
+    });
+  }),
+  500
+);
