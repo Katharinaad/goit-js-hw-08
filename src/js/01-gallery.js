9c1bf460f67b109b665cc6ad56452070974cf153
Katharinaad <vermilion.adams@gmail.com>
2023-07-30 21:00:49 +0200

    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const ulGallery = document.querySelector('.gallery');
+const markup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      item => `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`
+    )
+    .join('');
+}
+
+ulGallery.insertAdjacentHTML('beforeend', markup);
+
+let galleryLightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 200,
+});
