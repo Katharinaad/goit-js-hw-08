6e251ddd80a27aa8a09361cede56596967fa1a4b
Katharinaad <vermilion.adams@gmail.com>
2023-08-01 14:33:02 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,24 +4,29 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-let savedTime = localStorage.getItem('videoplayer-current-time');
-if (savedTime != null) {
-  player.setCurrentTime(savedTime);
-
-  console.log(savedTime);
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 }
+const currentTime = localStorage.getItem('videoplayer-current-time') || 0;
+player.setCurrentTime(currentTime);
 
-player.on(
-  'pause',
-  throttle(function (data) {
-    console.log('paused the video!');
+// let savedTime = localStorage.getItem('videoplayer-current-time');
+// if (savedTime != null) {
+//   player.setCurrentTime(savedTime);
 
-    player.getCurrentTime().then(function (seconds) {
-      localStorage.setItem(
-        'videoplayer-current-time',
-        JSON.stringify(data.seconds)
-      );
-    });
-  }),
-  500
-);
+//   console.log(savedTime);
+// }
+
+// player.on(
+//   'pause',
+//   throttle(function (data) {
+//     player.getCurrentTime().then(function (seconds) {
+//       localStorage.setItem(
+//         'videoplayer-current-time',
+//         JSON.stringify(data.seconds)
+//       );
+//     });
+//   }),
+//   500
+// );
