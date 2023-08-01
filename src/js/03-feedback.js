139484fa30e33c6019de714ab765258ec82318c2
Katharinaad <vermilion.adams@gmail.com>
2023-08-01 07:57:21 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+// const textarea = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('submit', formSubmit);
+// textarea.addEventListener('input', inputTextarea);
+
+initForm();
+
+// let emailValue = '';
+// let messageValue = '';
+
+const formObj = {
+  // email: emailValue,
+  // message: messageValue,
+};
+
+// prevent default
+function formSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  formData.forEach((value, name) => console.log(value, name));
+
+  event.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+form.addEventListener('input', throttle(onInputForm, 500));
+
+function onInputForm(event) {
+  formObj[event.target.name] = event.target.value;
+
+  const stringifiedData = JSON.stringify(formObj);
+  localStorage.setItem('feedback-form-state', stringifiedData);
+}
+
+// when loading the page, checking localstorage for values, if data is present
+function initForm() {
+  let savedForm = localStorage.getItem('feedback-form-state');
+  if (savedForm) {
+    savedForm = JSON.parse(savedForm);
+    Object.entries(savedForm).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }
+}
+
+// -------------------------------------------------------------------------------
+
+// form.addEventListener('input', event => {
+//   formObj[event.target.name] = event.target.value;
+
+//   const stringifiedData = JSON.stringify(formObj);
+//   localStorage.setItem('feedback-form-state', stringifiedData);
+// });
